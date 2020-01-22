5cd65bcd34424f49b735ddd88c44df9943a3e039
applelyntan <e0323585@u.nus.edu>
2020-01-22 17:25:35 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,19 +6,27 @@ public class Duke {
         System.out.println("Hello! I'm Applelyn\n" +
                 "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> textList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
         String command = scanner.nextLine();
         while (!command.equals("bye")) {
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 int number = 1;
-                for (String text: textList) {
-                    System.out.println(number + ". "
-                    + text);
+                for (Task task: taskList) {
+                    System.out.println(number + ". [" + task.getStatusIcon()
+                            + "] " + task.description);
                     number++;
                 }
+            } else if (command.contains("done")) {
+                int position = command.indexOf(" ");
+                int rank = Integer.valueOf(command.substring(position+1));
+                taskList.get(rank-1).doneTask();
+                System.out.println("Nice! I've marked this task as done:\n" +
+                        "[" + taskList.get(rank-1).getStatusIcon() + "] " +
+                        taskList.get(rank-1).description);
             } else {
                 System.out.println("added: " + command);
-                textList.add(command);
+                taskList.add(new Task(command));
             }
             command = scanner.nextLine();
         }
