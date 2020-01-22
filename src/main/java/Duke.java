e7649986b9693a12e6728d41fac0580f238918e5
applelyntan <e0323585@u.nus.edu>
2020-01-22 15:31:37 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,11 +4,11 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Applelyn\n" +
                 "What can I do for you?");
-        Scanner sc = new Scanner(System.in);
-        String command = sc.next();
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.next();
         while (!command.equals("bye")) {
             System.out.println(command);
-            command = sc.next();
+            command = scanner.next();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
