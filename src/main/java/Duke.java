a2972dc81e200bc445d59f63be6e5ce4adb136e8
applelyntan <e0323585@u.nus.edu>
2020-01-22 15:51:43 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Applelyn\n" +
                 "What can I do for you?");
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.next();
+        ArrayList<String> textList = new ArrayList<>();
+        String command = scanner.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(command);
-            command = scanner.next();
+            if (command.equals("list")) {
+                int number = 1;
+                for (String text: textList) {
+                    System.out.println(number + ". "
+                    + text);
+                    number++;
+                }
+            } else {
+                textList.add(command);
+                System.out.println("added: " + command);
+            }
+            command = scanner.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
