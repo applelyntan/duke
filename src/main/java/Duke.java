fbed6df681a7dbc97ebb3afb8ff4ed3fa996f408
applelyntan <e0323585@u.nus.edu>
2020-01-22 15:28:04 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Applelyn\n" +
+                "What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.next();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = sc.next();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
